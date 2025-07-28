b3f78ad64bc557806d254dd0f0b80a092b246dfd
Karl Ludwig Weise <ludwig@ludwigweise.de>
2025-07-28 21:24:48 +0200

    Fix prettier

--- src/lib/components/ui/command/command-group.svelte
+++ src/lib/components/ui/command/command-group.svelte
@@ -22,9 +22,7 @@
 	{...restProps}
 >
 	{#if heading}
-		<CommandPrimitive.GroupHeading
-			class="text-muted-foreground px-2 py-1.5 text-xs font-medium"
-		>
+		<CommandPrimitive.GroupHeading class="text-muted-foreground px-2 py-1.5 text-xs font-medium">
 			{heading}
 		</CommandPrimitive.GroupHeading>
 	{/if}
