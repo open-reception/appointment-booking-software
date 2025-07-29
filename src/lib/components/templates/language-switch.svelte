af8fee2299d22626870695a1af57497324b9eba6
Karl Ludwig Weise <ludwig@ludwigweise.de>
2025-07-29 15:22:16 +0200

    Added language switch template to satisfy pipeline

---
+++ src/lib/components/templates/language-switch.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { m } from "$i18n/messages.js";
+	import { getLocale, setLocale } from "$i18n/runtime.js";
+	import { ComboBox } from "$lib/components/ui/combobox";
+</script>
+
+<div>
+	<ComboBox
+		options={[
+			{ label: "Deutsch", value: "de", keywords: ["german", "deutsch"] },
+			{ label: "English", value: "en", keywords: ["english"] }
+		]}
+		value={getLocale()}
+		onChange={(value) => {
+			setLocale(value as "de" | "en");
+		}}
+		labels={{
+			placeholder: m["i18n.label"](),
+			search: m["i18n.search"](),
+			notFound: m["i18n.notFound"]()
+		}}
+	/>
+</div>
