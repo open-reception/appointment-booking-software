ea5a7e50ce78a75d68c3adf8e1502f5393a51f6f
Hendrik Belitz <hendrik@innovation-through-understanding.de>
2025-09-07 15:51:54 +0200

    67 - Refactored backend errors, added additional guards for tenant creation

--- src/lib/server/utils/errors.ts
+++ src/lib/server/utils/errors.ts
@@ -1,19 +1,51 @@
-export class AuthenticationError extends Error {
-  constructor(message: string) {
+import { json } from "@sveltejs/kit";
+
+export class BackendError extends Error {
+  constructor(
+    message: string,
+    public code: number,
+  ) {
     super(message);
+  }
+
+  public toJson = () => json({ message: this.message }, { status: this.code });
+}
+
+export class AuthenticationError extends BackendError {
+  constructor(
+    message: string,
+    public code = 403,
+  ) {
+    super(message, code);
     this.name = "AuthenticationError";
   }
 }
-export class ValidationError extends Error {
-  constructor(message: string) {
-    super(message);
+export class ValidationError extends BackendError {
+  constructor(
+    message: string,
+    public code = 422,
+  ) {
+    super(message, code);
     this.name = "ValidationError";
   }
 }
 
-export class NotFoundError extends Error {
-  constructor(message: string) {
-    super(message);
+export class NotFoundError extends BackendError {
+  constructor(
+    message: string,
+    public code = 404,
+  ) {
+    super(message, code);
     this.name = "ValidationError";
+  }
+}
+
+export class ConflictError extends BackendError {
+  constructor(
+    message: string,
+    public code = 409,
+  ) {
+    super(message, code);
+    this.name = "ConflictError";
   }
 }
