8ec4fcf37eefd8ba23c112fada373697ea81ffd8
Karl Ludwig Weise <office@ludwigweise.de>
2025-11-12 14:30:07 +0100

    Pages for managing my account set up (#131)
    Co-authored-by: Karl Ludwig Weise <ludwig@ludwigweise.de>

---
+++ src/routes/(pages)/dashboard/account/change-email/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { m } from "$i18n/messages";
+  import { MaxPageWidth } from "$lib/components/layouts/max-page-width";
+  import { SidebarLayout } from "$lib/components/layouts/sidebar-layout";
+  import { Headline } from "$lib/components/ui/typography";
+  import { ROUTES } from "$lib/const/routes";
+</script>
+
+<SidebarLayout
+  breakcrumbs={[
+    {
+      label: m["nav.account"](),
+      href: ROUTES.DASHBOARD.ACCOUNT.MAIN,
+    },
+    {
+      label: m["account.change-email.navItem"](),
+      href: ROUTES.DASHBOARD.ACCOUNT.CHANGE_EMAIL,
+    },
+  ]}
+>
+  <MaxPageWidth maxWidth="md" class="flex flex-col gap-6">
+    <Headline level="h1" style="h3">{m["account.change-email.title"]()}</Headline>
+  </MaxPageWidth>
+</SidebarLayout>
---
+++ src/routes/(pages)/dashboard/account/general/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { m } from "$i18n/messages";
+  import { MaxPageWidth } from "$lib/components/layouts/max-page-width";
+  import { SidebarLayout } from "$lib/components/layouts/sidebar-layout";
+  import { Headline } from "$lib/components/ui/typography";
+  import { ROUTES } from "$lib/const/routes";
+</script>
+
+<SidebarLayout
+  breakcrumbs={[
+    {
+      label: m["nav.account"](),
+      href: ROUTES.DASHBOARD.ACCOUNT.MAIN,
+    },
+    {
+      label: m["account.general.navItem"](),
+      href: ROUTES.DASHBOARD.ACCOUNT.GENERAL,
+    },
+  ]}
+>
+  <MaxPageWidth maxWidth="md" class="flex flex-col gap-6">
+    <Headline level="h1" style="h3">{m["account.general.title"]()}</Headline>
+  </MaxPageWidth>
+</SidebarLayout>
