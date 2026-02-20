0e79ba1717b6e24fe4d76e2f1f5997ec07d58521
Karl Ludwig Weise <ludwig@ludwigweise.de>
2026-02-20 20:33:48 +0100

    Staff: Grant access to existing clients front-end implementation -wip

--- src/routes/(pages)/dashboard/staff/(components)/grant-access-form/grant-access-form.svelte
+++ src/routes/(pages)/dashboard/staff/(components)/grant-access-form/grant-access-form.svelte
@@ -5,26 +5,51 @@
   import { InlineCode } from "$lib/components/ui/inline-code";
   import { TranslationWithComponent } from "$lib/components/ui/translation-with-component";
   import { Text } from "$lib/components/ui/typography";
+  import type { ClientTunnelResponse } from "$lib/server/services/appointment-service";
   import { auth } from "$lib/stores/auth";
   import type { TStaff } from "$lib/types/users";
   import StopIcon from "@lucide/svelte/icons/octagon-x";
   import { toast } from "svelte-sonner";
+  import { addStaffKeyShares, fetchClientTunnels } from "../utils";
+  import { Check, TriangleAlert } from "@lucide/svelte";
 
   let { entity, done }: { entity: TStaff; done: () => void } = $props();
   const myUserRole = $derived($auth.user?.role);
+  const tenantId = $derived($auth.user?.tenantId);
+  let step: "init" | "fetch-tunnels" | "add-staff-key-shares" | "success" | "error" =
+    $state("init");
   let isSubmitting = $state(false);
+  let tunnels: ClientTunnelResponse[] = $state([]);
 
-  const grantAccess = () => {
+  const grantAccess = async () => {
+    if (!tenantId) return;
+
     isSubmitting = true;
+    step = "fetch-tunnels";
 
-    // Simulate an async operation
-    setTimeout(() => {
+    try {
+      // Fetching tunnels
+      tunnels = await fetchClientTunnels(tenantId);
+      step = "add-staff-key-shares";
+
+      // Setting staff key shares
+      const isOk = await addStaffKeyShares(tenantId, entity.id, tunnels);
+      if (isOk) {
+        step = "success";
+        isSubmitting = false;
+        toast.success(m["staff.access.success.title"]());
+        done();
+      } else {
+        step = "error";
+        toast.error(m["staff.access.error.title"]());
+        isSubmitting = false;
+      }
+    } catch (error) {
+      console.error(error);
+      step = "error";
+      toast.error(m["staff.access.error.title"]());
       isSubmitting = false;
-      toast.success(m["staff.access.success"]());
-      done();
-    }, 2000);
-
-    // toast.error(m["staff.access.error"]());
+    }
   };
 </script>
 
@@ -42,21 +67,34 @@
       Icon={StopIcon}
       size="sm"
     />
-  {:else}
-    <div class="mt-6 flex flex-col gap-4">
-      {#if isSubmitting}
-        <CenterLoadingState label={m["staff.access.loading"]({ success: 0, total: 1 })} />
-      {/if}
-      <Button
-        size="lg"
-        type="button"
-        onclick={grantAccess}
-        isLoading={isSubmitting}
-        disabled={isSubmitting}
-      >
-        {m["staff.access.action"]()}
-      </Button>
-    </div>
+  {:else if step === "init"}
+    <Button
+      size="lg"
+      type="button"
+      onclick={grantAccess}
+      isLoading={isSubmitting}
+      disabled={isSubmitting}
+    >
+      {m["staff.access.action"]()}
+    </Button>
+  {:else if step === "fetch-tunnels"}
+    <CenterLoadingState label={m["staff.access.loadingTunnels"]()} />
+  {:else if step === "add-staff-key-shares"}
+    <CenterLoadingState label={m["staff.access.loadingAddingKeyShares"]()} />
+  {:else if step === "success"}
+    <CenterState
+      headline={m["staff.access.success.title"]()}
+      description={m["staff.access.success.description"]({ name: entity.name })}
+      Icon={Check}
+      size="sm"
+    />
+  {:else if step === "error"}
+    <CenterState
+      headline={m["staff.access.error.title"]()}
+      description={m["staff.access.error.description"]()}
+      Icon={TriangleAlert}
+      size="sm"
+    />
   {/if}
 </div>
 
