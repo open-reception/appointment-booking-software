36104d21ee0b6616f1c10273ec9970a27cb56b57
Karl Ludwig Weise <ludwig@ludwigweise.de>
2026-05-05 10:53:59 +0200

    Sanitize and limit fe log messages

--- src/routes/api/log/+server.ts
+++ src/routes/api/log/+server.ts
@@ -5,23 +5,36 @@ export async function POST({ request }) {
   try {
     const { level, message, meta } = await request.json();
 
+    // Block messages that are bigger than 4KB to prevent abuse
+    if (typeof message === "string" && message.length > 4096) {
+      logger.warn("Blocked oversized log message from client", { length: message.length });
+      return json({ success: false, error: "Message too long" }, { status: 400 });
+    }
+
     const clientLogger = logger.setContext("CLIENT");
+
+    const sanitizedMessage = `${message}`
+      .replace(/\n/g, "\\n")
+      .replace(/\r/g, "\\r")
+      .replace(/\0/g, "\\0")
+      // eslint-disable-next-line no-control-regex
+      .replace(/[\x01-\x09\x0B\x0C\x0E-\x1F\x7F]/g, "");
 
     switch (level) {
       case "debug":
-        clientLogger.debug(message, meta);
+        clientLogger.debug(sanitizedMessage, meta);
         break;
       case "info":
-        clientLogger.info(message, meta);
+        clientLogger.info(sanitizedMessage, meta);
         break;
       case "warn":
-        clientLogger.warn(message, meta);
+        clientLogger.warn(sanitizedMessage, meta);
         break;
       case "error":
-        clientLogger.error(message, meta);
+        clientLogger.error(sanitizedMessage, meta);
         break;
       default:
-        clientLogger.info(message, meta);
+        clientLogger.info(sanitizedMessage, meta);
     }
 
     return json({ success: true });
